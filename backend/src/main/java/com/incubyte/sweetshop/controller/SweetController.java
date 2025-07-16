4d301462d8e225e7c89ac434ae0544a6f343e4a0
Preksha <prekshadivraniya09@gmail.com>
2025-07-17 01:27:57 +0530

    Implemented add sweet endpoint and also tested it through Postman

--- backend/src/main/java/com/incubyte/sweetshop/controller/SweetController.java
+++ backend/src/main/java/com/incubyte/sweetshop/controller/SweetController.java
@@ -1,7 +1,10 @@
 package com.incubyte.sweetshop.controller;
 
+import com.incubyte.sweetshop.model.Sweet;
 import com.incubyte.sweetshop.service.SweetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,14 @@ public class SweetController {
 
     @Autowired
     private SweetService sweetService;
+
+    @PostMapping("/add")
+    public Sweet addSweet(@RequestBody Sweet sweet) {
+        return sweetService.addSweet(
+                sweet.getName(),
+                sweet.getCategory(),
+                sweet.getPrice(),
+                sweet.getQuantityInStock()
+        );
+    }
 }
